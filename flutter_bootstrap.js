8d17e209906b043941afe7073467a2bd78807557
Roberio Albino <roberio.ip@gmail.com>
2026-05-31 01:41:47 -0500

    deploy: carrossel de banners

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "886923139" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3276542969" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
